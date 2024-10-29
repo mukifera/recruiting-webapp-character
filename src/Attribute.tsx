964d48eba407ef715dec42706a3e1b4d904c3afe
Feras Muki <feras.muki@gmail.com>
2024-10-28 23:45:31 -0400

    implement modifiers

--- src/Attribute.tsx
+++ src/Attribute.tsx
@@ -1,10 +1,11 @@
 
 function Attribute({ name, value, update }) {
+  const modifier = Math.floor((value - 10) / 2)
   return (
     <li>
-      {name}
+      {name}: {value}
+      (Modifier: {modifier})
       <button onClick={() => update(1)}>+</button>
-      {value}
       <button onClick={() => update(-1)}>-</button>
     </li>
   );
